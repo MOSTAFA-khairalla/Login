9c2c9f2edf5232cd9343865636f945b9838cd8d8
Mostafa <Mostafa@DESKTOP-6G8HHFP>
2024-04-20 03:10:57 +0200

    Add project files.

---
+++ Login/Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Login.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
